15a0914b1f869d796d732bc710b043c95f7832ff
ksami <ksami.ihide@gmail.com>
2016-01-12 00:32:55 +0800

    have sidebar of functions to drag from

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,7 @@ var _ = require('underscore');
 var rev = require('underscore.string/reverse');
 var pipette = require('./pipette');
 
+//TODO: get sortable order of functions
 function run(){
     var text = _.map(document.getElementById('inputtext').value.split('\n'), str=>str.trim());
     var command = _.map(document.getElementById('commandtext').value.split('|'), str=>str.trim());
@@ -19,22 +20,49 @@ document.getElementById('clickme').onclick = run;
 
 
 $(function(){
-  $( ".column" ).sortable({
-    connectWith: ".column",
-    handle: ".portlet-header",
-    cancel: ".portlet-toggle",
-    placeholder: "portlet-placeholder ui-corner-all"
+  $('#chain').sortable({
+    // connectWith:'.column",
+    handle: '.portlet-header',
+    cursor: 'move',
+    cancel: '.portlet-toggle',
+    placeholder: 'portlet-placeholder ui-corner-all',
+    items: '> .portlet:not(:last)',
+    receive: function(ev, ui){
+      $item = $(ui.helper[0]);
+      $item.toggleClass('ui-draggable');
+      $item.removeAttr('style');
+      $item.find('.portlet-toggle').click(function() {
+        var icon = $( this );
+        icon.toggleClass('ui-icon-minusthick ui-icon-plusthick');
+        icon.closest('.portlet').find('.portlet-content').toggle();
+      });
+    }
   });
   
-  $( ".portlet" )
-    .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-    .find( ".portlet-header" )
-      .addClass( "ui-widget-header ui-corner-all" )
-      .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+  $('#sidebar > .portlet').draggable({
+    connectToSortable: '#chain',
+    helper: 'clone',
+    handle: '.portlet-header',
+    cursor: 'move',
+    revert: 'invalid'
+  });
+
+
+  $('.portlet')
+    .addClass('ui-widget ui-widget-content ui-helper-clearfix ui-corner-all')
+    .find('.portlet-header')
+      .addClass('ui-widget-header ui-corner-all')
+      .prepend('<span class="ui-icon ui-icon-minusthick portlet-toggle"></span>');
+
+  $('#sidebar .portlet-content').toggle();
+  $('#sidebar .portlet-next-arrow').toggle();
+  $('#sidebar .portlet-toggle').toggleClass('ui-icon-minusthick ui-icon-plusthick');
+  $('.portlet').disableSelection();
+
   
-  $( ".portlet-toggle" ).click(function() {
+  $('.portlet-toggle').click(function() {
     var icon = $( this );
-    icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-    icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    icon.toggleClass('ui-icon-minusthick ui-icon-plusthick');
+    icon.closest('.portlet').find('.portlet-content').toggle();
   });
 });
